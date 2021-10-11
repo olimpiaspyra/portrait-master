3ddb5d122dc4fc4f905caee6a7234137bd7ca82f
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-10-11 21:33:22 +0200

    Fix bug in app

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo.model');
+const Voter = require('../models/Voter.model');
 
 /****** SUBMIT PHOTO ********/
 
@@ -11,18 +12,33 @@ exports.add = async (req, res) => {
     if(title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const fileExt = fileName.split('.').slice(-1)[0];
 
+      const titlePattern = new RegExp(/^([A-z]|\s|[0-9]|\.){1,25}$/);
+      if(!titlePattern.test(title)) throw new Error('Invalid title pattern...');
+
+      const authorPattern = new RegExp(/^([A-z]|\s|[0-9]|\.){1,50}$/);
+      if(!authorPattern.test(author)) throw new Error('Invalid author pattern...');
+
+      const emailPattern = new RegExp(/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/);
+      if(!emailPattern.test(email)) throw new Error('Invalid email pattern...');
+
+      if(fileExt === 'gif' || fileExt === 'jpg' || fileExt === 'png') {
+
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+
+      } else {
+        throw new Error ('Wrong file!');
+      } 
     } else {
       throw new Error('Wrong input!');
-    }
-
-  } catch(err) {
-    res.status(500).json(err);
+    } 
   }
-
+    catch(err) {
+      res.status(500).json(err);
+    }    
 };
 
 /****** LOAD ALL PHOTOS ********/
@@ -44,7 +60,20 @@ exports.vote = async (req, res) => {
   try {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
+
     else {
+      const voter = await Voter.findOne({ user: req.clientIp });
+      if (!voter) {
+        const newVoter = new Voter({ user: req.clientIp, votes: [ photoToUpdate._id ] });
+        await newVoter.save();
+      } else {
+        if (voter.votes.includes(photoToUpdate._id)) {
+          throw new Error('Vote duplicate!');
+        } else {
+          voter.votes.push(photoToUpdate._id);
+          await voter.save();
+        }
+      }
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
